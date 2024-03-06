214243c43c9bb1366b550018d1f45d9f4b8e76ca
Samuel-Oli-P <samuel.pereira@sptech.school>
2024-03-06 10:56:16 -0300

    feature: base mobile foodway

---
+++ foodway/app/src/main/java/com/example/foodway/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.foodway
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.foodway.screens.ProfileCustomerScreen
+import com.example.foodway.ui.theme.FoodwayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainApp()
+        }
+    }
+}
+
+@Composable
+fun MainApp() {
+    FoodwayTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            ProfileCustomerScreen()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MainApp()
+}
