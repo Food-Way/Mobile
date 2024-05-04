8f8e786118f4fdd0400bc3a1028fec84fc0ff475
felipyroma <felipy.roma@sptech.school>
2024-05-03 22:07:42 -0300

    adjust in pages and page of information of user created

--- foodway/app/src/main/java/com/example/foodway/view/login/Login.kt
+++ foodway/app/src/main/java/com/example/foodway/view/login/Login.kt
@@ -1,148 +1,148 @@
-package com.example.foodway.view.login
-
-
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.foodway.R
-import com.example.foodway.view.components.ButtonGeneric
-import com.example.foodway.view.components.InputGeneric
-
-@Composable
-fun Login(
-    onNavigate: () -> Unit = {}
-) {
-    val coroutineScope = rememberCoroutineScope()
-    Box(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Box(
-            modifier = Modifier
-                .width(350.dp)
-                .height(650.dp)
-                .padding(16.dp)
-                .border(2.dp, Color.LightGray, RoundedCornerShape(16.dp))
-        ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.foodway_logo),
-                    contentDescription = stringResource(id = R.string.logo),
-                    modifier = Modifier
-                        .width(95.dp)
-                        .height(98.dp)
-                )
-                Spacer(modifier = Modifier.height(10.dp))
-
-                Text(
-                    text = stringResource(id = R.string.welcome_title),
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 18.sp
-                )
-
-                InputGeneric(
-                    inputLabel = stringResource(id = R.string.email),
-                    icon = R.drawable.email_icon,
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Email
-                    )
-                )
-
-                Spacer(modifier = Modifier.height(15.dp))
-
-                InputGeneric(
-                    inputLabel = stringResource(id = R.string.password),
-                    icon = R.drawable.lock_icon,
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Password
-                    )
-                )
-
-                Spacer(modifier = Modifier.height(40.dp))
-
-                ButtonGeneric(
-                    text = stringResource(id = R.string.enter),
-                    modifier = Modifier
-                        .width(270.dp)
-                        .height(43.dp),
-                    isPrimary = true
-                ) {}
-
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Text(
-                    text = stringResource(id = R.string.forget_password),
-                    fontSize = 15.sp,
-                    modifier = Modifier.clickable {
-                    }
-                )
-
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Box(
-                    modifier = Modifier
-                        .border(1.dp, Color.LightGray)
-                        .width(270.dp)
-                        .height(1.dp)
-                )
-
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Text(
-                    text = stringResource(id = R.string.havent_account),
-                    fontSize = 15.sp,
-                )
-                Spacer(modifier = Modifier.height(20.dp))
-                ButtonGeneric(
-                    text = stringResource(id = R.string.signup),
-                    modifier = Modifier
-                        .width(130.dp)
-                        .height(43.dp),
-                    isPrimary = false,
-//                    onClick = {
-//                        coroutineScope.launch {
+//package com.example.foodway.view.login
 //
-//                        }
-//                    },
-                ) {
-                    onNavigate()
-                }
-            }
-        }
-    }
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Login()
-}
+//
+//import androidx.compose.foundation.Image
+//import androidx.compose.foundation.border
+//import androidx.compose.foundation.clickable
+//import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.Spacer
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.layout.width
+//import androidx.compose.foundation.shape.RoundedCornerShape
+//import androidx.compose.foundation.text.KeyboardOptions
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.rememberCoroutineScope
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.res.painterResource
+//import androidx.compose.ui.res.stringResource
+//import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.text.input.KeyboardType
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.unit.sp
+//import com.example.foodway.R
+//import com.example.foodway.view.components.ButtonGeneric
+//import com.example.foodway.view.components.InputGeneric
+//
+//@Composable
+//fun Login(
+//    onNavigate: () -> Unit = {}
+//) {
+//    val coroutineScope = rememberCoroutineScope()
+//    Box(
+//        modifier = Modifier
+//            .fillMaxSize(),
+//        contentAlignment = Alignment.Center
+//    ) {
+//        Box(
+//            modifier = Modifier
+//                .width(350.dp)
+//                .height(650.dp)
+//                .padding(16.dp)
+//                .border(2.dp, Color.LightGray, RoundedCornerShape(16.dp))
+//        ) {
+//            Column(
+//                modifier = Modifier.fillMaxSize(),
+//                horizontalAlignment = Alignment.CenterHorizontally,
+//                verticalArrangement = Arrangement.Center
+//            ) {
+//                Image(
+//                    painter = painterResource(id = R.drawable.foodway_logo),
+//                    contentDescription = stringResource(id = R.string.logo),
+//                    modifier = Modifier
+//                        .width(95.dp)
+//                        .height(98.dp)
+//                )
+//                Spacer(modifier = Modifier.height(10.dp))
+//
+//                Text(
+//                    text = stringResource(id = R.string.welcome_title),
+//                    fontWeight = FontWeight.Bold,
+//                    fontSize = 18.sp
+//                )
+//
+//                InputGeneric(
+//                    inputLabel = stringResource(id = R.string.email),
+//                    icon = R.drawable.email_icon,
+//                    keyboardOptions = KeyboardOptions(
+//                        keyboardType = KeyboardType.Email
+//                    )
+//                )
+//
+//                Spacer(modifier = Modifier.height(15.dp))
+//
+//                InputGeneric(
+//                    inputLabel = stringResource(id = R.string.password),
+//                    icon = R.drawable.lock_icon,
+//                    keyboardOptions = KeyboardOptions(
+//                        keyboardType = KeyboardType.Password
+//                    )
+//                )
+//
+//                Spacer(modifier = Modifier.height(40.dp))
+//
+//                ButtonGeneric(
+//                    text = stringResource(id = R.string.enter),
+//                    modifier = Modifier
+//                        .width(270.dp)
+//                        .height(43.dp),
+//                    isPrimary = true
+//                ) {}
+//
+//                Spacer(modifier = Modifier.height(20.dp))
+//
+//                Text(
+//                    text = stringResource(id = R.string.forget_password),
+//                    fontSize = 15.sp,
+//                    modifier = Modifier.clickable {
+//                    }
+//                )
+//
+//                Spacer(modifier = Modifier.height(20.dp))
+//
+//                Box(
+//                    modifier = Modifier
+//                        .border(1.dp, Color.LightGray)
+//                        .width(270.dp)
+//                        .height(1.dp)
+//                )
+//
+//                Spacer(modifier = Modifier.height(20.dp))
+//
+//                Text(
+//                    text = stringResource(id = R.string.havent_account),
+//                    fontSize = 15.sp,
+//                )
+//                Spacer(modifier = Modifier.height(20.dp))
+//                ButtonGeneric(
+//                    text = stringResource(id = R.string.signup),
+//                    modifier = Modifier
+//                        .width(130.dp)
+//                        .height(43.dp),
+//                    isPrimary = false,
+////                    onClick = {
+////                        coroutineScope.launch {
+////
+////                        }
+////                    },
+//                ) {
+//                    onNavigate()
+//                }
+//            }
+//        }
+//    }
+//
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    Login()
+//}
--- foodway/app/src/main/java/com/example/foodway/view/register/StepOneRegisterUser.kt
+++ foodway/app/src/main/java/com/example/foodway/view/register/StepOneRegisterUser.kt
@@ -1,4 +1,4 @@
-package com.example.foodway.view.edit.customer
+package com.example.foodway.view.register
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -16,20 +16,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.foodway.R
-import com.example.foodway.model.CustomerInputManager.personalCustomerInputInfos
+import com.example.foodway.model.CustomerInputManager
 import com.example.foodway.view.components.ButtonGeneric
 import com.example.foodway.view.components.InputGeneric
-
-
 @Composable
-fun EditCustomerAccount() {
+fun StepOneRegisterUser(){
     Column(
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
@@ -37,34 +35,6 @@ fun EditCustomerAccount() {
             .fillMaxWidth()
             .fillMaxSize()
     ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-
-        ) {
-            Column {
-                Text(
-                    text = stringResource(id = R.string.edit_account),
-                    fontWeight = FontWeight.ExtraBold,
-                    fontSize = 18.sp,
-                )
-
-                Text(
-                    text = stringResource(id = R.string.adjust_necessary),
-                    fontSize = 15.sp,
-                )
-            }
-
-            Image(
-                painter = painterResource(id = R.drawable.foodway_logo),
-                contentDescription = stringResource(id = R.string.logo),
-                modifier = Modifier
-                    .width(95.dp)
-                    .height(98.dp)
-            )
-        }
 
         Spacer(modifier = Modifier.height(30.dp))
 
@@ -72,12 +42,12 @@ fun EditCustomerAccount() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            items(personalCustomerInputInfos.size) { item ->
+            items(CustomerInputManager.personalCustomerInputInfos.size) { item ->
                 InputGeneric(
-                    inputLabel = personalCustomerInputInfos[item].inputLabel,
-                    icon = personalCustomerInputInfos[item].icon,
+                    inputLabel = CustomerInputManager.personalCustomerInputInfos[item].inputLabel,
+                    icon = CustomerInputManager.personalCustomerInputInfos[item].icon,
                     keyboardOptions = KeyboardOptions(
-                        keyboardType = personalCustomerInputInfos[item].type
+                        keyboardType = CustomerInputManager.personalCustomerInputInfos[item].type
                     )
                 )
 
@@ -93,11 +63,11 @@ fun EditCustomerAccount() {
             modifier = Modifier.fillMaxWidth()
         ) {
             ButtonGeneric(
-                text = stringResource(id = R.string.save),
+                text = stringResource(id = R.string.next),
                 modifier = Modifier
                     .width(300.dp)
                     .height(43.dp),
-                isPrimary = true
+                isPrimary = false
             ) {}
         }
 
@@ -106,6 +76,6 @@ fun EditCustomerAccount() {
 
 @Preview(showBackground = true)
 @Composable
-fun EditAccountPreview() {
-    EditCustomerAccount()
+fun StepOneRegisterUserPreview() {
+    StepOneRegisterUser()
 }
